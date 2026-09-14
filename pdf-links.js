e05b27226060b1531c2003f0d9402d6bb0d469d2
punnada <punnada.mim@gmail.com>
2026-09-14 16:28:00 +0700

    Initial WebGIS upload

---
+++ pdf-links.js
@@ -0,0 +1,796 @@
+window.PDF_FILES = [
+
+/* =====================================================
+   กระบี่ / เกาะลันตา
+===================================================== */
+
+{
+    province:"กระบี่",
+    district:"เกาะลันตา",
+    subdistrict:"เกาะกลาง",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1M8QJJP4i08UL04K1ygII-FFJSvTKnRWT/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1AgLNDA3pYKc_2U7jZ3_SwvvQ4y6JWE1z/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เกาะลันตา",
+    subdistrict:"เกาะลันตาน้อย",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1rNwcxqbrX6unE3zsvcqMJuQr_HXOJgrh/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1C4VpasQnMHfnesyHPkk33-8Crctvx5UN/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เกาะลันตา",
+    subdistrict:"เกาะลันตาใหญ่",
+
+    forestUrls:[
+        "https://drive.google.com/file/d/1dY9nvW8pb7yjGStlO1Z7LHWObGIPm1eU/view"
+    ],
+
+    parkUrls:[],
+
+    mapUrls:[]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เกาะลันตา",
+    subdistrict:"ศาลาด่าน",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1XMFxvaKrcNyXVGV6G6c5PY7NQajI2sdO/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เกาะลันตา",
+    subdistrict:"คลองยาง",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1rNRZowl8dk2SEktP6Dgvcj6E4T27Rw4L/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+/* =====================================================
+   กระบี่ / คลองท่อม
+===================================================== */
+
+{
+    province:"กระบี่",
+    district:"คลองท่อม",
+    subdistrict:"พรุดินนา",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1uEI3mRLiEzT6tnPw_eA1DZZsNW0buTsx/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1DYSvJjBm6IJVdrFIvwtW8tJLziq_4fXz/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"คลองท่อม",
+    subdistrict:"คลองท่อมเหนือ",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/15NS6oyGEvgGCjBq2M_0DS3R8dA3pVR0g/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1vKyRbeTDgw7mkWxLCwhtQR8_bU2HGTJy/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"คลองท่อม",
+    subdistrict:"คลองท่อมใต้",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1dSRZEsbwxXC428I1OreeSSZextqNJ5s0/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1wh3ltRSNeKGHv0Voie-26QDUGvP4g_rB/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"คลองท่อม",
+    subdistrict:"ห้วยน้ำขาว",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1t0VBcAYN5qboZ5-QRIf9r-t1X6XMqr1t/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1hINKwtjjayYJNJd1Z6_68mh8u6frdP3V/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"คลองท่อม",
+    subdistrict:"ทรายขาว",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1rElAFeYgzBTqBySJLzD5lWeKy2tngOGA/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1_ZOWDVKDrERiodizuwWvJ6zJbIOmAN5I/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"คลองท่อม",
+    subdistrict:"เพหลา",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1Jo_U2Aph1EZZAJvmiQd_pBfOCQFvhQPK/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1YcWE5EbHO4Fn_BeALq9BmWXxqindNB_V/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"คลองท่อม",
+    subdistrict:"คลองพน",
+
+    forestUrls:[],
+
+    parkUrls:[],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1oM05OYFwLnze5FaaxNhNwZFiLsrTQiQB/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"คลองท่อม",
+    subdistrict:"คลองพนพัฒนา",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1_s8t1DPUv_f96RuiHO2HS57Y8z8hFhGb/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+/* =====================================================
+   กระบี่ / เขาพนม
+===================================================== */
+
+{
+    province:"กระบี่",
+    district:"เขาพนม",
+    subdistrict:"เขาดิน",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1wMMpGmRLdk8D0muHq9n4znDorg3IZ_4j/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1fnreX8GOHlcXz9PDd-mYnAkdQywY04Qs/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เขาพนม",
+    subdistrict:"สินปุน",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1nz3AcCKq8bToRahH6qMhgYW3V2ZCrPgS/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1ueaHNO-OAzARuQDh9Er3VwPQUuEPMdET/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เขาพนม",
+    subdistrict:"พรุเตียว",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/10Ue_4g0Rx3Fv6pXUCOzAJmebSRzY-IID/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1HiRsmj0rMKWoWAx3_m2VoeOqb8cgPI3A/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เขาพนม",
+    subdistrict:"โคกหาร",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1khJCbU2YZlDWU5TGAvy-7D_X4DY3pcH8/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1pjQnvxgWtvi4sNuMhi3S_3sqvl4JBZp3/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เขาพนม",
+    subdistrict:"หน้าเขา",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1KiFUYZWgWxgK2KTYCVjhhNXQoAHCY1zY/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/13O4P6dT6hZea1vXnbkll7DrdH5PY2H6a/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เขาพนม",
+    subdistrict:"เขาพนม",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1Qyp70gNeGb_npAtnszr2SKsM01nwYyEm/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+/* =====================================================
+   กระบี่ / เมือง
+===================================================== */
+
+{
+    province:"กระบี่",
+    district:"เมือง",
+    subdistrict:"กระบี่น้อย",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1b_WtTNvry2li5mPN_ODJYO4XPmoYnHdf/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1zE1CrAY7iu3bNKYXiLo-AvMP6Le1pW_3/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เมือง",
+    subdistrict:"เขาคราม",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/14c7vpmVkWQdbZOxxmuMpXFLgTkZW1pJi/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1QM7x9Xd0P2Q1g1YfpDbHd4I5dcQivfLQ/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เมือง",
+    subdistrict:"เขาทอง",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1Cz-50Lhf99vAD5pQ0fDXVVkoNLoXWvWZ/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/10WJuFh_0uuV_qhZy8XXUGadP-8FYzaLS/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เมือง",
+    subdistrict:"ทับปริก",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/13UzGChECLKDXa_-2JApDgplE9wJx3lDv/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1oK_vDnsz91PboVwdb6Feu_Y7561G0ApV/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เมือง",
+    subdistrict:"ไสไทย",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1NWwwyCHumZKVMomOzxDCwolorbO7wCuI/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1AipYPB79BHPcG4sNVz22Tu8_afpNk2cn/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เมือง",
+    subdistrict:"คลองประสงค์",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1VK7FKhIMgaGNrzwBzUpj-9YbLA9xT_su/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เมือง",
+    subdistrict:"อ่าวนาง",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1a4nmzPmE5ffSF_JbQ86mSRXtEt6zItEY/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เมือง",
+    subdistrict:"หนองทะเล",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1JMsaKG5BV9YnNEYPzM7xx1FYBjjMnHki/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+/* =====================================================
+   กระบี่ / เหนือคลอง
+===================================================== */
+
+{
+    province:"กระบี่",
+    district:"เหนือคลอง",
+    subdistrict:"เกาะศรีบอยา",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1_tU-ZRVAJGpao-hdP120ubPlV_HIKcrd/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1ExlvYeiyBjfh00n4oImjswyPPHYO01N-/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เหนือคลอง",
+    subdistrict:"โคกยาง",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1fo6WcM6JgZ3zNoBbwtaztSARMq_Mw1bv/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1Dbey6cKRWCY57kMsPvoaQyYeWv4qLtLB/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เหนือคลอง",
+    subdistrict:"คลองขนาน",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/142a5-E8y6ArwlFNlR4eNJ8PyRooV4Y10/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/148plEvshS1OHoXbU2e3RSWdhFq8OOfqh/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เหนือคลอง",
+    subdistrict:"ตลิ่งชัน",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1-JCYu_fv1zC0lTDM17s522IcInrQoX93/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1FJii-FxPsCkUWmczh47boMxhqu13gyBh/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เหนือคลอง",
+    subdistrict:"คลองเขม้า",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1TiPh194x_Be7fJoFMHHBcsyxqzRNhXYZ/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เหนือคลอง",
+    subdistrict:"ปกาสัย",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1QFt-gy8IEW-_lreyBBdcq6NwtU-4NOPl/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เหนือคลอง",
+    subdistrict:"เหนือคลอง",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1gYFpTKrfEX9xdU99UwVF9Z8Doc2u1QMZ/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+{
+    province:"กระบี่",
+    district:"เหนือคลอง",
+    subdistrict:"ห้วยยูง",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1kEnEVheLGL1y1pvfdAypVCHeK54WwYQv/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+/* =====================================================
+   กระบี่ / ปลายพระยา
+===================================================== */
+
+{
+    province:"กระบี่",
+    district:"ปลายพระยา",
+    subdistrict:"เขาต่อ",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/10jgDwFzIF_x04LACkiz4-2Jv5qdhrqHn/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1mqznhlclL020XZEp6yaSbNkQnsUWGVnT/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"ปลายพระยา",
+    subdistrict:"เขาเขน",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1jP6w3p4q9X8ux_Xn_SB63z0DtRCRMS6S/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1LSVB-2j2W-SqomA7X9uFbgjB8AmVXNO0/view"
+    ]
+},
+
+
+/* =====================================================
+   กระบี่ / อ่าวลึก
+===================================================== */
+
+{
+    province:"กระบี่",
+    district:"อ่าวลึก",
+    subdistrict:"แหลมสัก",
+
+    forestUrls:[],
+
+    /*
+      ในกรมอุทยานมีไฟล์ชื่อ แหลมสัก
+      และ เทศบาลแหลมสัก
+      จึงเก็บได้ 2 ลิงก์
+    */
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1dFFICtXmzHBlUS-MfDYvKB7XFMXI3IHa/view",
+        "https://drive.google.com/file/d/1wQ7WK6AsI90verFSUIuBzYVvi8iq_HFH/view"
+    ],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/1-Yhk843erTHUUMavLV7FzE0VhgeQDU9O/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"อ่าวลึก",
+    subdistrict:"เขาใหญ่",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1OglGtL76-x5ty0vCZLs46nTwH26tpXig/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+{
+    province:"กระบี่",
+    district:"อ่าวลึก",
+    subdistrict:"นาเหนือ",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1WEhHWNVj2P_xbmk9CTqK6gtJSogtN_04/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+{
+    province:"กระบี่",
+    district:"อ่าวลึก",
+    subdistrict:"อ่าวลึกเหนือ",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/15Hfb9KiX2rExM05L1U-lBP7u-VYyGjWS/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+{
+    province:"กระบี่",
+    district:"อ่าวลึก",
+    subdistrict:"อ่าวลึกน้อย",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/13Utv05kZdiV1OPpy8oiJGdVssjjD1IiL/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+{
+    province:"กระบี่",
+    district:"อ่าวลึก",
+    subdistrict:"อ่าวลึกใต้",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1H_TyjkKxVfJMQ2EpV5ZD3w_WNchtL8_d/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+/* =====================================================
+   กระบี่ / ลำทับ
+===================================================== */
+
+{
+    province:"กระบี่",
+    district:"ลำทับ",
+    subdistrict:"ลำทับ",
+
+    forestUrls:[],
+
+    parkUrls:[],
+
+    mapUrls:[
+        "https://drive.google.com/file/d/13szUIlpZ7i6BUL3KdYSs-GQ2-nYm--B2/view"
+    ]
+},
+
+
+{
+    province:"กระบี่",
+    district:"ลำทับ",
+    subdistrict:"ทุ่งไทรทอง",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1Jz1Iwi1pSw9WuFW6C1usFXEGyw-DTOBl/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+{
+    province:"กระบี่",
+    district:"ลำทับ",
+    subdistrict:"ทับลำ",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1Vwm1lUyTfg-qOj0uEiYkMsZPEpYMftwW/view"
+    ],
+
+    mapUrls:[]
+},
+
+
+{
+    province:"กระบี่",
+    district:"ลำทับ",
+    subdistrict:"ดินแดง",
+
+    forestUrls:[],
+
+    parkUrls:[
+        "https://drive.google.com/file/d/1liagwpeLfcIyGuPbSbAXHhI17saqO59Q/view"
+    ],
+
+    mapUrls:[]
+}
+
+];
